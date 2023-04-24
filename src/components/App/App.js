e3a0ff581641f8945f44d38b52a7fbb56ac7ac70
Katerina <tkachenko.city@gmail.com>
2023-04-24 21:52:10 +0300

    LocalStorage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,6 +6,15 @@ import ContactList from 'components/ContactList/ContactList';
 import Filter from 'components/Filter/Filter';
 import { Layout } from 'components/Layout/Layout';
 
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
+// render > didMount > getItem > setState > update > render > didUpdate > setItem
+
 export class App extends Component {
   state = {
     contacts: [
@@ -16,6 +25,23 @@ export class App extends Component {
     ],
     filter: '',
   };
+
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts !== null) {
+      // Если сохранили в LS уже что-то, пишем ЭТО в state
+      this.setState({ contacts: JSON.parse(savedContacts) });
+    } else {
+      // Если в LS ничего еще нет, пишем в state initialRecipes
+      this.setState({ contacts: initialContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   handleAddContact = ({ name, number }) => {
     const { contacts } = this.state;
