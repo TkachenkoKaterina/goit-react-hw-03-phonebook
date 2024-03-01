b4dcb03d9b32e2c7803a883b9003da936f8aba2e
Katerina <90516132+TkachenkoKaterina@users.noreply.github.com>
2024-03-01 17:21:29 +0200

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { nanoid } from 'nanoid';
+import ContactForm from '../ContactForm/ContactForm';
+import ContactList from '../ContactList/ContactList';
+import Filter from '../Filter/Filter';
+import css from './App.module.css';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  handleFilter = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  handleSubmit = contact => {
+    const { name } = contact;
+    const { contacts } = this.state;
+
+    const isDuplicate = contacts.some(
+      existingContact =>
+        existingContact.name.toLowerCase() === name.toLowerCase()
+    );
+
+    if (isDuplicate) {
+      alert(`${name} is already in contacts!`);
+    } else {
+      this.setState(prevState => ({
+        contacts: [{ id: nanoid(), ...contact }, ...prevState.contacts],
+      }));
+    }
+  };
+
+  handleDeleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  render() {
+    const { filter, contacts } = this.state;
+
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+
+    return (
+      <div className={css.container}>
+        <h1 className={css.titleFirst}>Phonebook</h1>
+        <ContactForm onSubmit={this.handleSubmit} />
+        <h2 className={css.titleFSecond}>Contacts</h2>
+        <Filter value={filter} onChange={this.handleFilter} />
+        <ContactList
+          contacts={filteredContacts}
+          onDeleteContact={this.handleDeleteContact}
+        />
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  filter: PropTypes.string,
+};
